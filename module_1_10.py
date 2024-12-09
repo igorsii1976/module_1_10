fe44173b0e0c033012ce74846423c99c11944990
IgorShcherbina <51137776+igorsii1976@users.noreply.github.com>
2024-12-09 08:58:50 +0300

    Add files via upload

---
+++ module_1_10.py
@@ -0,0 +1,17 @@
+print()
+
+#Вводные данные
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+#Сортировка учеников в алфавитном порядке
+students = sorted(students)
+
+#Определяем средний балл
+a = [(sum(grades[0]) / len(grades[0])),(sum(grades[1]) / len(grades[1])),(sum(grades[2]) / len(
+grades[2])),(sum(grades[3]) / len(grades[3])),(sum(grades[4]) / len(grades[4]))]
+
+#Формируем словарь, где ключ - имя ученика, а значением - его средний балл
+glossary = {students[0]: a[0], students[1]: a[1], students[2]: a[2],
+            students[3]: a[3], students[4]: a[4]}
+print("Журнал успеваемости:",glossary)
